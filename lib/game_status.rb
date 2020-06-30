bfdabf63a7f8b946961329bebab70e227730406c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-30 23:30:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,26 @@ WIN_COMBINATIONS = [
   [0,4,8], # Diagonal down
   [2,4,6], # Diagonal up
 ]
+
+def won?(array)
+  WIN_COMBINATIONS.each do |win_combination|
+    # grab the index combination of winnin positions, assign to 3 index variables
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    
+    # load the values of the board at each winning position for this iteration
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    # assign array with the postitions to check
+    win_positions[position_1, position_2, position_3]
+  
+    win_positions.all? do |marker|
+      marker == "X" || marker == "O"
+    end 
+    
+  end
+end
